26f9e650885de02347b6759fb07dd69d4c038e3a
nitishkumarmaurya <nitishkumarmaurya820.hitit2020@gmail.com>
2023-04-20 19:12:50 +0530

    second commit

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,28 +3,16 @@ package com.example.calculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.ZeroCornerSize
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
+import androidx.compose.material.TextField
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.calculator.ui.theme.CalculatorTheme
 import kotlinx.coroutines.launch
 import androidx.compose.material.Text as Text1
 
@@ -32,101 +20,109 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val scaffoldState= rememberScaffoldState()
-            var textFieldState by remember{
-                mutableStateOf("")
-            }
-            var textFieldState1 by remember{
-                mutableStateOf("")
-            }
-            var textFieldState2 by remember{
-                mutableStateOf("")
-            }
-            val scope= rememberCoroutineScope()
-            Scaffold(modifier = Modifier.fillMaxSize(),
-                scaffoldState=scaffoldState
-            ) {
-                Column (horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(horizontal = 30.dp)
-                ){
-                    Text1(text = "Calculator",
-                        fontSize = 50.sp
-                        )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    TextField(
-                        value = textFieldState,
-                        label = {
-                            Text1(text = "Enter the first number")
-                        },
-                        onValueChange = {
-                            textFieldState=it
-                        },
-                        singleLine = true,
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    TextField(
-                        value = textFieldState1,
-                        label = {
-                            Text1(text = "Enter the second number")
-                        },
-                        onValueChange = {
-                            textFieldState1=it
-                        },
-                        singleLine = true,
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    TextField(
-                        value = textFieldState2,
-                        label = {
-                            Text1(text = "Enter any one operation from(+,-,*,/)")
-                        },
-                        onValueChange = {
-                            textFieldState2=it
-                        },
-                        singleLine = true,
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Button(onClick = {
-                        scope.launch {
-                             val answer: Int
-                            if (textFieldState2.equals("+")) {
-                                answer=(Integer.parseInt(textFieldState)+Integer.parseInt(textFieldState1))
-                                scaffoldState.snackbarHostState.showSnackbar("$answer")
-                            }
-                            else if(textFieldState2.equals("-"))
-                            {
-                                answer=(Integer.parseInt(textFieldState)-Integer.parseInt(textFieldState1))
-                                scaffoldState.snackbarHostState.showSnackbar("$answer")
-                            }
-                            else if(textFieldState2.equals("*"))
-                            {
-                                answer=(Integer.parseInt(textFieldState)*Integer.parseInt(textFieldState1))
-                                scaffoldState.snackbarHostState.showSnackbar("$answer")
-                            }
-                            else
-                            {
-                                if(textFieldState1.equals("0")){
-                                    answer=0
-                                    scaffoldState.snackbarHostState.showSnackbar("Division by 0")
-                                }
+            Operation()
+        }
+    }
+}
+@Composable
+fun Operation(){
+    val scaffoldState = rememberScaffoldState()
+    var textFieldState by remember {
+        mutableStateOf("")
+    }
+    var textFieldState1 by remember {
+        mutableStateOf("")
+    }
+    var textFieldState2 by remember {
+        mutableStateOf("")
+    }
+    val scope = rememberCoroutineScope()
 
-                                else {
-                                    answer = (Integer.parseInt(textFieldState) / Integer.parseInt(textFieldState1))
-                                    scaffoldState.snackbarHostState.showSnackbar("$answer")
-                                }
-                            }
-
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        scaffoldState = scaffoldState
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 30.dp)
+        ) {
+            Text1(
+                text = "Calculator",
+                fontSize = 50.sp
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            TextField(
+                value = textFieldState,
+                label = {
+                    Text1(text = "Enter the first number")
+                },
+                onValueChange = {
+                    textFieldState = it
+                },
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            TextField(
+                value = textFieldState1,
+                label = {
+                    Text1(text = "Enter the second number")
+                },
+                onValueChange = {
+                    textFieldState1 = it
+                },
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            TextField(
+                value = textFieldState2,
+                label = {
+                    Text1(text = "Enter any one operation from(+,-,*,/)")
+                },
+                onValueChange = {
+                    textFieldState2 = it
+                },
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = {
+                scope.launch {
+                    val answer: Int
+                    if (textFieldState2.equals("+")) {
+                        answer = (Integer.parseInt(textFieldState) + Integer.parseInt(
+                            textFieldState1
+                        ))
+                        scaffoldState.snackbarHostState.showSnackbar("$answer")
+                    } else if (textFieldState2.equals("-")) {
+                        answer = (Integer.parseInt(textFieldState) - Integer.parseInt(
+                            textFieldState1
+                        ))
+                        scaffoldState.snackbarHostState.showSnackbar("$answer")
+                    } else if (textFieldState2.equals("*")) {
+                        answer = (Integer.parseInt(textFieldState) * Integer.parseInt(
+                            textFieldState1
+                        ))
+                        scaffoldState.snackbarHostState.showSnackbar("$answer")
+                    } else {
+                        if (textFieldState1.equals("0")) {
+                            answer = 0
+                            scaffoldState.snackbarHostState.showSnackbar("Division by 0")
+                        } else {
+                            answer = (Integer.parseInt(textFieldState) / Integer.parseInt(
+                                textFieldState1
+                            ))
+                            scaffoldState.snackbarHostState.showSnackbar("$answer")
                         }
-                    }) {
-                        Text1(text = "Submit")
                     }
+
                 }
+            }) {
+                Text1(text = "Submit")
             }
         }
     }
